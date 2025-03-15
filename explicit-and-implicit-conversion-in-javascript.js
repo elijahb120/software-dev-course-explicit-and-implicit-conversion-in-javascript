0656f9556441040a37df1a14b2656bec2bbeca47
Eli <70232706+elijahb120@users.noreply.github.com>
2025-03-15 09:48:31 -0500

    L3 P1 Update explicit-and-implicit-conversion-in-javascript.js

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -22,11 +22,25 @@ Use console.log() to clearly show the before-and-after type conversions.
 let result = "5" - 2;
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean(false); // I removed the quotations around "false" because those made the value truthy. Now that it just says false, the value is falsey
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
+let age = Number("25"); // I used the number conversion to signify that 25 should read as a number, not a string. Allowing it to perform addition and not mash numbers together.
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+//Example of implicit type conversion
+
+if("You Passed Go") {
+    console.log("Collect $200!") ; // Will run
+}
+if(NaN) {
+    console.log("Do not collect $200") ; // Will not run
+}
+
+//Example of explicit type conversion
+
+let str = "25.32 miles" ;
+console.log((parseInt(str)) + "ish miles") ;
